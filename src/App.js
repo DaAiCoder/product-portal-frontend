609dbad66bbbfc451fb23d43d2a21dac261f1edb
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-10 17:25:23 -0400

    fix(frontend): correct App.js syntax and add /feeds route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 // File: src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 import Feeds from './pages/Feeds';
-<Routes>
-  <Route path="/login" element={<Login />} />
-  <Route
-    path="/"
-    element={
-      <ProtectedRoute><Home /></ProtectedRoute>
-    }
-  />
-+ <Route
-+   path="/feeds"
-+   element={
-+     <ProtectedRoute><Feeds /></ProtectedRoute>
-+   }
-+ />
-  <Route
-    path="/profile"
-    element={
-      <ProtectedRoute><Profile /></ProtectedRoute>
-    }
-  />
-</Routes>
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/feeds"
+            element={
+              <ProtectedRoute>
+                <Feeds />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
