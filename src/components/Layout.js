30462b82950d690d55fef379e188d0f5f5dc10d9
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-11 15:52:56 -0400

    chore: add link to Theme Editor in main navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-// File: src/components/Layout.js
+// File: src/ components/Layout.js
 import React, { useEffect, useState, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
