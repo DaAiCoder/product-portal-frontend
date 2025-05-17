b54e5f3df1d594ff3a2755c2313ec8acb63701bc
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-17 17:19:47 -0400

    Fix: remove duplicate widgetLibrary declaration

--- src/utils/widgetLibrary.js
+++ src/utils/widgetLibrary.js
@@ -1,4 +1,3 @@
-// File: src/utils/widgetLibrary.js
 import datetime from '../components/widgets/configs/datetime';
 import stopwatch from '../components/widgets/configs/stopwatch';
 import quote from '../components/widgets/configs/quote';
@@ -17,48 +16,4 @@ export const widgetLibrary = [
   chat,
   calculator,
   worldclock,
-];
-
-
-export const widgetLibrary = [
-  {
-    ...datetime,
-    defaultW: 4,
-    defaultH: 4,
-  },
-  {
-    ...stopwatch,
-    defaultW: 4,
-    defaultH: 4,
-  },
-  {
-    ...quote,
-    defaultW: 4,
-    defaultH: 3,
-  },
-  {
-    ...weather,
-    defaultW: 4,
-    defaultH: 4,
-  },
-  {
-    ...aisummary,
-    defaultW: 4,
-    defaultH: 5,
-  },
-  {
-    ...chat,
-    defaultW: 6,
-    defaultH: 8,
-  },
-  {
-    ...calculator,
-    defaultW: 4,
-    defaultH: 5,
-  },
-  {
-    ...worldclock,
-    defaultW: 4,
-    defaultH: 6,
-  },
 ];
