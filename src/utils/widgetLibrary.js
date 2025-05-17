324ed0cf9e552cc03b7e4719a2563180b2d7a2e6
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-17 16:14:27 -0400

    Fix widgetLibrary: add defaultW/defaultH to all widgets

--- src/utils/widgetLibrary.js
+++ src/utils/widgetLibrary.js
@@ -9,12 +9,44 @@ import calculator from '../components/widgets/configs/calculator';
 import worldclock from '../components/widgets/configs/worldclock';
 
 export const widgetLibrary = [
-  datetime,
-  stopwatch,
-  quote,
-  weather,
-  aisummary,
-  chat,
-  calculator,
-  worldclock,
+  {
+    ...datetime,
+    defaultW: 4,
+    defaultH: 4,
+  },
+  {
+    ...stopwatch,
+    defaultW: 4,
+    defaultH: 4,
+  },
+  {
+    ...quote,
+    defaultW: 4,
+    defaultH: 3,
+  },
+  {
+    ...weather,
+    defaultW: 4,
+    defaultH: 4,
+  },
+  {
+    ...aisummary,
+    defaultW: 4,
+    defaultH: 5,
+  },
+  {
+    ...chat,
+    defaultW: 6,
+    defaultH: 8,
+  },
+  {
+    ...calculator,
+    defaultW: 4,
+    defaultH: 5,
+  },
+  {
+    ...worldclock,
+    defaultW: 4,
+    defaultH: 6,
+  },
 ];
