206707a37f0af4163bbbf9b2c220de203faac4d2
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-17 20:10:03 -0400

    Fix widgetLibrary to include icon and label for all widgets

--- src/utils/widgetLibrary.js
+++ src/utils/widgetLibrary.js
@@ -1,3 +1,6 @@
+// File: src/utils/widgetLibrary.js
+import { FaClock, FaStopwatch, FaQuoteRight, FaCloudSun, FaRobot, FaComments, FaCalculator, FaGlobe } from 'react-icons/fa';
+
 import datetime from '../components/widgets/configs/datetime';
 import stopwatch from '../components/widgets/configs/stopwatch';
 import quote from '../components/widgets/configs/quote';
@@ -8,12 +11,60 @@ import calculator from '../components/widgets/configs/calculator';
 import worldclock from '../components/widgets/configs/worldclock';
 
 export const widgetLibrary = [
-  datetime,
-  stopwatch,
-  quote,
-  weather,
-  aisummary,
-  chat,
-  calculator,
-  worldclock,
+  {
+    ...datetime,
+    icon: <FaClock size={40} />,
+    label: 'Date & Time',
+    w: 4,
+    h: 4,
+  },
+  {
+    ...stopwatch,
+    icon: <FaStopwatch size={40} />,
+    label: 'Stopwatch',
+    w: 4,
+    h: 4,
+  },
+  {
+    ...quote,
+    icon: <FaQuoteRight size={40} />,
+    label: 'Quote of the Day',
+    w: 4,
+    h: 4,
+  },
+  {
+    ...weather,
+    icon: <FaCloudSun size={40} />,
+    label: 'Weather',
+    w: 4,
+    h: 4,
+  },
+  {
+    ...aisummary,
+    icon: <FaRobot size={40} />,
+    label: 'AI Summary',
+    w: 4,
+    h: 4,
+  },
+  {
+    ...chat,
+    icon: <FaComments size={40} />,
+    label: 'Chat',
+    w: 4,
+    h: 5,
+  },
+  {
+    ...calculator,
+    icon: <FaCalculator size={40} />,
+    label: 'Calculator',
+    w: 4,
+    h: 4,
+  },
+  {
+    ...worldclock,
+    icon: <FaGlobe size={40} />,
+    label: 'World Clock',
+    w: 4,
+    h: 4,
+  },
 ];
