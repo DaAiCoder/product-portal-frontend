0e248c55b9e100517c889ad38fce40e77f90d5a3
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-25 19:37:35 -0400

    fix timerwidget import

---
+++ src/components/widgets/configs/timer.js
@@ -0,0 +1,14 @@
+//Product-portal-frontend\src\components\widgets\configs\time.js
+
+import { FaClock } from 'react-icons/fa';
+import TimeWidget from '../TimerWidget';
+
+export default {
+  id: 'time',
+  name: 'Time',
+  icon: FaClock,
+  component: TimerWidget,
+  defaultConfig: {
+    refreshInterval: 60000, // update every minute for clock & stopwatch
+  },
+};
