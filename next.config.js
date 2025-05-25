af18b2835c3f6542fbeab6dc164411ee9f34c27a
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-25 16:39:53 -0400

    Fix chrono-node ESM build by routing locales to CJS bundle

--- next.config.js
+++ next.config.js
@@ -1,38 +1,18 @@
 // next.config.js
-const path = require('path');
 const webpack = require('webpack');
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: {
-    esmExternals: false,   // prefer CJS externals
-  },
+  experimental: { esmExternals: false },
   webpack(config) {
-    // 1) Prefer "main" (CJS) over "module" (ESM)
+    // Tell Webpack to prefer the "main" (CJS) field
     config.resolve.mainFields = ['main', 'module'];
 
-    // 2) Alias the package root to the CJS build
-    config.resolve.alias['chrono-node$'] = path.resolve(
-      __dirname,
-      'node_modules/chrono-node/dist/chrono-node.js'
-    );
-
-    // 3) Also alias any deep esm imports to that same CJS file
-    config.resolve.alias['chrono-node/dist/esm'] = path.resolve(
-      __dirname,
-      'node_modules/chrono-node/dist/chrono-node.js'
-    );
-
-    // 4) And catch any locale refiners under esm
+    // Replace any import of chrono-node/dist/esm/locales/... with the base chrono-node CJS
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(
-        /chrono-node\/dist\/esm\/.*$/,
-        resource => {
-          resource.request = path.resolve(
-            __dirname,
-            'node_modules/chrono-node/dist/chrono-node.js'
-          );
-        }
+        /chrono-node\/dist\/esm\/locales\/.*\.js$/,
+        'chrono-node'
       )
     );
 
