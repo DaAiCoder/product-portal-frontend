b5fbac2ee0dc08f12d21c935e2e23479a7e377a1
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-05-31 16:57:10 -0400

    Secure FeedsPage: use JWT for topic/articles access

--- src/pages/feeds.js
+++ src/pages/feeds.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './feeds.css';
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
 
