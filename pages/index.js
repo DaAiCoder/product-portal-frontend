e4810fc3c89a05f7441697e500863932a8c3dcaf
DaAiCoder <tresdeddd-crypto@yahoo.com>
2025-06-01 21:53:18 -0400

    Set home page to render Login component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 // /pages/index.js
-export default function Home() {
-  return <div style={{fontSize: "2rem", padding: 32}}>HELLO FROM INDEX.JS</div>;
+
+import Login from './Login';
+
+export default function Home(props) {
+  return <Login {...props} />;
 }
